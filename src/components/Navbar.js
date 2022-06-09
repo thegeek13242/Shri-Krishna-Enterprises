f04f64a6134853232c9d514d67670527049ad44a
Aviral Verma <bt20cse214@iiitn.ac.in>
2022-06-09 18:10:13 +0530

    Fix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,18 +7,8 @@ import Typography from "@mui/material/Typography";
 
 import Container from "@mui/material/Container";
 
-// const pages = ["About Us", "Brands", "Bank Details", "Enquiry"];
 
 const ResponsiveAppBar = () => {
-  const [anchorElNav, setAnchorElNav] = React.useState(null);
-
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
-
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
 
   return (
     <AppBar position="fixed">
@@ -41,44 +31,7 @@ const ResponsiveAppBar = () => {
           >
             Shri Krishna Enterprises
           </Typography>
-          {/* 
-          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton> */}
-          {/* <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: "bottom",
-                horizontal: "left",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "left",
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: "block", md: "none" },
-              }}
-            > */}
-          {/* {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">{page}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box> */}
-
+          
           <Typography
             variant="h5"
             noWrap
@@ -97,17 +50,7 @@ const ResponsiveAppBar = () => {
           >
             Shri Krishna Enterprises
           </Typography>
-          {/* <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-            {pages.map((page) => (
-              <Button
-                key={page}
-                onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: "white", display: "block" }}
-              >
-                {page}
-              </Button>
-            ))}
-          </Box> */}
+
         </Toolbar>
       </Container>
     </AppBar>
